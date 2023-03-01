7170e76df4044941f134d5a4b6ebfed7007a678a
BOMI CHOI <bnewbom@gmail.com>
2023-03-01 13:58:30 +0900

    도서 수정

--- pages/admin/books/modify/[id].jsx
+++ pages/admin/books/modify/[id].jsx
@@ -1,64 +1,30 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Card1 from "components/Card1";
-import Image from "next/image";
-import { Delete } from "@mui/icons-material";
 import {
   Grid,
   Box,
   Button,
-  Card,
   Divider,
-  IconButton,
-  MenuItem,
   Stack,
   TextField,
 } from "@mui/material";
 import * as Yup from "yup";
 import { Formik } from "formik";
-
-import DropZone from "components/DropZone";
-import { FlexBox } from "components/flex-box";
 import { H3, Paragraph } from "components/Typography";
 import VendorDashboardLayout from "components/layouts/admin-dashboard";
 import axios from "axios";
 
-const INITIAL_BOOKS = {};
-const INITIAL_USER = {};
-
-const validationIsbn = Yup.object().shape({
-  Isbn: Yup.string()
-    .required("ISBN 또는 도서명을 입력하세요.")
-    .matches(/^[0-9]+$/, "10자리 또는 13자리 숫자 ISBN을 입력하세요.")
-    .test(
-      "len",
-      "10자리 또는 13자리 숫자 ISBN을 입력하세요.",
-      val => val.length === 10 || val.length === 13
-    ),
-});
-
-const validationSchema = Yup.object().shape({
-  Name: Yup.string().required("Shop Name is required!"),
-  shopPhone: Yup.string().required("Shop Phone is required!"),
-  category: Yup.string().required("Category is required!"),
-  description: Yup.string().required("Description is required!"),
-  shopAddress: Yup.string().required("Shop Address is required!"),
-  order: Yup.number().required("Orders is required!"),
-}); // =============================================================================
+let MODIFY_BOOK = {};
 
 ShopSettings.getLayout = function getLayout(page) {
   return <VendorDashboardLayout>{page}</VendorDashboardLayout>;
 }; // =============================================================================
 
 export default function ShopSettings() {
-  const [isbn, setIsbn] = useState("");
-  const [tradeData, setTradeData] = useState(null);
-  const [bookInfo, setBookInfo] = useState({});
-  const [bookCard, setBookCard] = useState(false);
-  const [nickname, setNickname] = useState("");
-  const [validIsbn, setValidIsbn] = useState("");
+  const [bookInfo, setBookInfo] = useState(null);
+  const [isbnInfo, setIsbnInfo] = useState(null);
   const [isLoading, setLoading] = useState(false);
-  const [sellStatus, setSellStatus] = useState("");
   const router = useRouter();
 
   useEffect(() => {
@@ -67,54 +33,24 @@ export default function ShopSettings() {
     getTrade(router.query.id);
   }, [router]);
 
-  const checkoutSchema = Yup.object().shape({
-    trade_title: Yup.string().required("책 제목을 입력하세요."),
-    sell_price: Yup.string().required("가격을 입력하세요."),
-  });
-
-  const handleOnChange = e => {
-    setIsbn(e.target.value);
-  };
-
-  const handleIsbnSubmit = values => {
-    getBookByIsbn(values.Isbn);
-  };
-
-  const handleUserSubmit = values => {
-    getUserId(values.userid);
-  };
-
-  const handleFormSubmit = values => {
-    const newObj = {
-      trade_title: values.trade_title,
-      seller_uid: parseInt(user_id),
-      sell_price: parseInt(values.sell_price),
-      trade_description: values.trade_description,
-    };
-    // setBookCard((prevState) => ({
-    //   ...prevState,
-    //   trade_title: values.trade_title,
-    //   sell_price: parseInt(values.sell_price),
-    //   trade_description: values.trade_description,
-    // }));
-    bookCard = { ...bookCard, ...newObj, ...bookInfo };
-    postNewTrade();
-  };
-
   const getTrade = trade_uid => {
     fetch(
       `https://i9nwbiqoc6.execute-api.ap-northeast-2.amazonaws.com/test/trade/${trade_uid}`
     )
       .then(res => res.json())
       .then(data => {
-        bookCard = { ...bookCard, ...data[0] };
-        setTradeData(data[0]);
         setBookInfo(data[0]);
-        setSellStatus(data[0].sell_state);
-        console.log(data, bookCard);
+        MODIFY_BOOK = data[0]
+        if (data[0].isbn) {
+          getBookByIsbn(data[0].isbn)
+        }
         setLoading(false);
       });
   };
+
+  const checkoutSchema = Yup.object().shape({
+    isbn: Yup.string().required("ISBN을 검색하세요."),
+  });
 
   const getBookByIsbn = async isbn => {
     await axios
@@ -122,11 +58,13 @@ export default function ShopSettings() {
         `https://i9nwbiqoc6.execute-api.ap-northeast-2.amazonaws.com/test/isbn?isbn=${isbn}`
       )
       .then(response => {
-        console.log(response.data.items[0]);
-        if (response.data.items[0] !== undefined) {
-          console.log(response.data.items[0]);
-          setBookInfo(response.data.items[0]);
-          setValidIsbn(isbn);
+        if (response.data.items.length > 0) {
+          setIsbnInfo(response.data.items[0]);
+          MODIFY_BOOK = { ...MODIFY_BOOK, ...response.data.items[0] }
+          postNewBook(response.data.items[0])
+        } else {
+          alert('책 정보가 없습니다. 다시 검색하세요.')
+          setBookInfo({ ...bookInfo, isbn: null })
         }
       })
       .catch(error => {
@@ -134,48 +72,15 @@ export default function ShopSettings() {
       });
   };
 
-  const getUserId = async id => {
-    await axios
-      .get(
-        `https://i9nwbiqoc6.execute-api.ap-northeast-2.amazonaws.com/test/user/${id}`
-      )
-      .then(response => {
-        console.log(response);
-        if (response.status === 200) {
-          setUserInfo(response.data[0]);
-        }
-      })
-      .catch(error => {
-        console.log(error);
-        // if(error.response.status === 401){
-        //   alert('이미 가입되어 있는 이메일 주소입니다.')
-        // }
-      });
-  };
-
-  const postNewBook = async () => {
+  const postNewBook = async (obj) => {
     await axios
       .post(
         `https://i9nwbiqoc6.execute-api.ap-northeast-2.amazonaws.com/test/book`,
-        {
-          isbn: validIsbn,
-          title: INITIAL_BOOKS.title,
-          author: INITIAL_BOOKS.author,
-          image: INITIAL_BOOKS.image,
-          price: INITIAL_BOOKS.price,
-          description: INITIAL_BOOKS.description,
-          publisher: INITIAL_BOOKS.publisher,
-          pubdate: INITIAL_BOOKS.pubdate,
-          // seller_uid: 15,
-          // sell_price: parseInt(values.sell_price),
-          // shop_uid: null,
-          // trade_description: values.trade_description,
-          // trade_image: null
-        }
+        obj
       )
       .then(response => {
         if (response.status === 200) {
-          postNewTrade(response.data);
+          MODIFY_BOOK = { ...MODIFY_BOOK, book_uid: response.data }
         }
       })
       .catch(error => {
@@ -183,19 +88,23 @@ export default function ShopSettings() {
       });
   };
 
-  const postNewTrade = async uid => {
+  const handleOnChange = e => {
+    setBookInfo({
+      ...bookInfo,
+      isbn: e.target.value
+    })
+  };
+
+  const handleIsbnSubmit = () => {
+    getBookByIsbn(bookInfo.isbn);
+  };
+
+  const putNewTrade = async () => {
+    console.log('ddd', MODIFY_BOOK)
     await axios
-      .post(
+      .put(
         `https://i9nwbiqoc6.execute-api.ap-northeast-2.amazonaws.com/test/trade`,
-        {
-          book_uid: uid,
-          seller_uid: INITIAL_USER.user_uid,
-          sell_state: "판매중",
-          sell_price: 2000,
-          shop_uid: null,
-          trade_description: `${nickname}님이 판매 등록한 도서입니다.`,
-          trade_image: null,
-        }
+        { ...MODIFY_BOOK, shop_uid: 1, book_condition: '최상' }
       )
       .then(response => {
         console.log(response);
@@ -209,38 +118,18 @@ export default function ShopSettings() {
       });
   };
 
-  const setBook = val => {
-    setBookCard(true);
-    INITIAL_BOOKS.title = val.title;
-    INITIAL_BOOKS.author = val.author;
-    INITIAL_BOOKS.publisher = val.publisher;
-    INITIAL_BOOKS.pubdate = val.pubdate;
-    INITIAL_BOOKS.price = val.discount;
-    INITIAL_BOOKS.description = val.description;
-    INITIAL_BOOKS.image =
-      val.image || "/assets/images/products/24.Revel2020.png";
-  };
-
-  const setUserInfo = val => {
-    // setUserStatus(true);
-    INITIAL_USER.user_uid = val.user_uid;
-    setNickname(val.nickname);
-    console.log(INITIAL_USER);
-  };
-
   return (
     <Box py={4} maxWidth={740} margin='auto'>
       <H3 mb={2}>도서 수정</H3>
 
       <Card1>
-        {tradeData && (
+        {bookInfo && (
           <>
             <Paragraph fontWeight={700} mb={2}>
               필수 입력 정보
             </Paragraph>
 
             <Formik
-              onSubmit={handleFormSubmit}
               initialValues={bookInfo}
               validationSchema={checkoutSchema}
             >
@@ -261,11 +150,8 @@ export default function ShopSettings() {
                           fullWidth
                           name='trade_title'
                           label='제목'
-                          onBlur={handleBlur}
-                          onChange={handleChange}
                           value={bookInfo.trade_title}
-                          error={!!touched.trade_title && !!errors.trade_title}
-                          helperText={touched.trade_title && errors.trade_title}
+                          readOnly
                         />
                       </Grid>
 
@@ -274,11 +160,7 @@ export default function ShopSettings() {
                           fullWidth
                           label='판매가'
                           name='sell_price'
-                          onBlur={handleBlur}
-                          value={bookInfo.sell_price}
                           onChange={handleChange}
-                          error={!!touched.sell_price && !!errors.sell_price}
-                          helperText={touched.sell_price && errors.sell_price}
                         />
                       </Grid>
 
@@ -289,18 +171,8 @@ export default function ShopSettings() {
                           fullWidth
                           color='info'
                           name='trade_description'
-                          onBlur={handleBlur}
-                          onChange={handleChange}
                           value={bookInfo.trade_description}
                           label='책소개'
-                          error={Boolean(
-                            errors.trade_description &&
-                              touched.trade_description
-                          )}
-                          helperText={
-                            touched.trade_description &&
-                            errors.trade_description
-                          }
                         />
                       </Grid>
                     </Grid>
@@ -324,16 +196,20 @@ export default function ShopSettings() {
                     <TextField
                       color='info'
                       type='text'
-                      name='Isbn'
+                      name='isbn'
                       label='ISBN'
                       value={bookInfo.isbn || ""}
                       onChange={handleOnChange}
+                      error={!!touched.isbn && !!errors.isbn}
+                      helperText={touched.isbn && errors.isbn}
                     />
                     <Button
                       onClick={handleIsbnSubmit}
                       color='primary'
                       variant='contained'
-                      disabled={isbn === ""}
+                      disabled={bookInfo.isbn === null}
+                      onChange={handleChange}
+
                     >
                       검색
                     </Button>
@@ -341,7 +217,7 @@ export default function ShopSettings() {
 
                   <Divider variant='middle' />
 
-                  {bookInfo && (
+                  {isbnInfo && (
                     <>
                       <Paragraph fontWeight={700} mt={2}>
                         도서 정보
@@ -350,7 +226,7 @@ export default function ShopSettings() {
                       <Stack spacing={3} mb={3}>
                         <img
                           name='image'
-                          src={bookInfo.image || ""}
+                          src={isbnInfo.image || ""}
                           style={{
                             width: "250px",
                             objectFit: "contain",
@@ -363,7 +239,7 @@ export default function ShopSettings() {
                           color='info'
                           name='title'
                           label='도서명'
-                          value={bookInfo.title || ""}
+                          value={isbnInfo.title || ""}
                           readOnly
                         />
 
@@ -372,7 +248,7 @@ export default function ShopSettings() {
                           name='publisher'
                           label='출판사'
                           onChange={handleChange}
-                          value={bookInfo.publisher || ""}
+                          value={isbnInfo.publisher || ""}
                         />
 
                         <TextField
@@ -382,7 +258,7 @@ export default function ShopSettings() {
                           placeholder='발행일'
                           label='발행일'
                           onChange={handleChange}
-                          value={bookInfo.pubdate || ""}
+                          value={isbnInfo.pubdate || ""}
                         ></TextField>
 
                         <TextField
@@ -392,7 +268,7 @@ export default function ShopSettings() {
                           color='info'
                           name='description'
                           onChange={handleChange}
-                          value={bookInfo.description || ""}
+                          value={isbnInfo.description || ""}
                           label='책소개'
                         />
 
@@ -401,26 +277,22 @@ export default function ShopSettings() {
                           name='price'
                           label='정가'
                           onChange={handleChange}
-                          value={bookInfo.price || ""}
+                          value={isbnInfo.discount || ""}
                         />
                       </Stack>
                     </>
                   )}
 
-                  <Button type='submit' variant='contained' color='primary'>
-                    저장
-                  </Button>
+                  {isbnInfo &&
+                    <Button onClick={putNewTrade} variant='contained' color='primary'>
+                      수정하기
+                    </Button>}
                 </form>
               )}
             </Formik>
           </>
         )}
       </Card1>
-      <Grid container justifyContent='flex-end'>
-        <Button color='info' variant='outlined' onClick={postNewBook}>
-          저장하기
-        </Button>
-      </Grid>
     </Box>
   );
 }
